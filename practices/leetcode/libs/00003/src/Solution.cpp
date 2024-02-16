f2d7c4454b14c7da3e93432591901cfd24a25380
xavier <i@xavierliu.io>
2024-02-17 02:43:31 +0800

    update leetcode problem 00003

--- practices/leetcode/libs/00003/src/Solution.cpp
+++ practices/leetcode/libs/00003/src/Solution.cpp
@@ -1,23 +1,23 @@
 #include "Solution.h"
 
-#include <set>
+#include <unordered_set>
 
 int Solution::lengthOfLongestSubstring(string s)
 {
-	return lengthOfLongestSubstring_recurse(s);
+    return lengthOfLongestSubstring_recurse(s);
 }
 /*
  * mark: 1/5
- * 无循环递归分割
+ * 无循环递归分割，究极无敌递归法，为了不循环而不循环，就是玩
  */
 int Solution::lengthOfLongestSubstring_recurse(string s, size_t mark)
 {
-	if (s.size() <= 1) return s.size() + mark;
+    if (s.size() <= 1) return s.size() + mark;
 
-	auto pos = s.find(s.front(), 1);
-	if (string::npos == pos) return lengthOfLongestSubstring_recurse(s.substr(1), 1 + mark);
+    auto pos = s.find(s.front(), 1);
+    if (string::npos == pos) return lengthOfLongestSubstring_recurse(s.substr(1), 1 + mark);
 
-	return max(lengthOfLongestSubstring_recurse(s.substr(0, pos), mark), lengthOfLongestSubstring_recurse(s.substr(1)));
+    return max(lengthOfLongestSubstring_recurse(s.substr(0, pos), mark), lengthOfLongestSubstring_recurse(s.substr(1)));
 }
 
 /*
@@ -26,25 +26,25 @@ int Solution::lengthOfLongestSubstring_recurse(string s, size_t mark)
  */
 int Solution::lengthOfLongestSubstring_dblPointer(string s)
 {
-	set<char> alpha;
-	size_t result = 0;
+    unordered_set<char> alpha;
+    size_t result = 0;
 
-	string::iterator left = s.begin();
-	string::iterator right = left;
+    string::iterator left = s.begin();
+    string::iterator right = left;
 
-	while (right != s.end())
-	{
-		if (alpha.end() == alpha.find(*right))
-		{
-			alpha.insert(*right);
-			++right;
-			continue;
-		}
-		result = max(result, alpha.size());
+    while (right != s.end())
+    {
+        if (alpha.end() == alpha.find(*right))
+        {
+            alpha.insert(*right);
+            ++right;
+            continue;
+        }
+        result = max(result, alpha.size());
 
-		alpha.erase(*left++);
-	}
-	return max(result, alpha.size());
+        alpha.erase(*left++);
+    }
+    return max(result, alpha.size());
 }
 
 /*
@@ -53,20 +53,20 @@ int Solution::lengthOfLongestSubstring_dblPointer(string s)
  */
 int Solution::lengthOfLongestSubstring_nomal(string s)
 {
-	set<char> alpha;
-	size_t result = 0;
+    unordered_set<char> alpha;
+    size_t result = 0;
 
-	for (string::iterator left = s.begin(); left != s.end(); ++left)
-	{
-		alpha.clear();
-		for (string::iterator right = left; right != s.end(); ++right)
-		{
-			if (alpha.end() != alpha.find(*right)) break;
+    for (string::iterator left = s.begin(); left != s.end(); ++left)
+    {
+        alpha.clear();
+        for (string::iterator right = left; right != s.end(); ++right)
+        {
+            if (alpha.end() != alpha.find(*right)) break;
 
-			alpha.insert(*right);
-		}
-		result = max(result, alpha.size());
-	}
+            alpha.insert(*right);
+        }
+        result = max(result, alpha.size());
+    }
 
-	return result;
+    return result;
 }
--- practices/leetcode/libs/00003/tests/main.cpp
+++ practices/leetcode/libs/00003/tests/main.cpp
@@ -8,27 +8,29 @@ using namespace ::testing;
 class NO_00003 : public testing::Test
 {
 protected:
-	void Check(std::string&& s, size_t answer)
-	{
-		ASSERT_EQ(solution.lengthOfLongestSubstring(s), answer);
-	}
+    void Check(std::string&& s, size_t answer)
+    {
+        ASSERT_EQ(solution.lengthOfLongestSubstring_nomal(s), answer) << s;
+        ASSERT_EQ(solution.lengthOfLongestSubstring_dblPointer(s), answer) << s;
+        ASSERT_EQ(solution.lengthOfLongestSubstring_recurse(s), answer) << s;
+    }
 
-	Solution solution;
+    Solution solution;
 };
 
 TEST_F(NO_00003, 0)
 {
-	Check("abcabcbb", 3);
+    Check("abcabcbb", 3);
 }
 TEST_F(NO_00003, 1)
 {
-	Check("bbbbb", 1);
+    Check("bbbbb", 1);
 }
 TEST_F(NO_00003, 2)
 {
-	Check("pwwkew", 3);
+    Check("pwwkew", 3);
 }
 TEST_F(NO_00003, 3)
 {
-	Check("dvdf", 3);
+    Check("dvdf", 3);
 }
